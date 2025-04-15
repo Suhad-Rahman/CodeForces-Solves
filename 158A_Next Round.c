79659601135a0cd92386aa7e915f18a3956aeef6
Suhad Rahman <suhadturan03@gmail.com>
2025-04-15 17:27:07 +0600

    Update 158A_Next Round.c
    Problem Link- https://codeforces.com/problemset/problem/158/A

--- 158A_Next Round.c
+++ 158A_Next Round.c
@@ -3,10 +3,10 @@ int main(){
 int n, k_th, i, a[50], eligible, Contestant=0;
 scanf("%d %d", &n, &k_th);
 
-for(i=1; i<=n; i++){
+for(i=0; i<=n; i++){
 scanf("%d", &a[i]);
 }
-eligible=a[k_th];
+eligible=a[k_th-1];
 
 for(i=1; i<=n; i++){
     if(0<a[i] && a[i]>=eligible){
