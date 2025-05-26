89159c325a297e237a4fc621e9a9f50d9771626c
Suhad Rahman <suhadturan03@gmail.com>
2025-05-27 03:16:43 +0600

    Add files via upload

---
+++ 977A - Wrong Subtraction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int n, k, i;
+scanf("%d %d", &n, &k);
+
+for(i=0; i<k; i++){
+    if(n%10!=0){
+        n--;
+    }
+    else{
+        n=n/10;
+    }
+}
+printf("%d", n);
+return 0;
+}
