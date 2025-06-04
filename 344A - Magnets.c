a22c48e59f68943c37f34bdffa14fb4a1d0f3b44
Suhad Rahman <suhadturan03@gmail.com>
2025-06-05 02:13:20 +0600

    Add files via upload

---
+++ 344A - Magnets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n, i, count=0;;
+    scanf("%d", &n);
+    int a[n];
+    
+    for(i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+    for(i=0; i<n; i++){
+        if(a[i]!=a[i+1]){
+            count++;
+        }
+    }        
+    printf("%d", count);
+    
+    return 0;
+}
