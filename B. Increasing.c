d36818ed101a1b4a200095bfd64fa9a97b4efaa6
Suhad Rahman <suhadturan03@gmail.com>
2025-10-09 00:53:22 +0600

    B. Increasing - Codeforces Round 827 (Div. 4)
    Problem Link: https://codeforces.com/contest/1742/problem/B
    
    Contest Link: https://codeforces.com/contest/1742

---
+++ B. Increasing.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    int t, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        flag=1;
+        int n;
+        scanf("%d", &n);
+        
+        
+        int a[n];
+        for(int i=0; i<n;i++){
+            scanf("%d", &a[i]);
+        }
+        
+        if(n==1){
+            printf("YES\n");
+        }
+        
+        else{
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+            if(a[i]==a[j]){
+                flag=0;
+                break;
+            }
+            else{
+                continue;
+                }
+            }
+
+        }
+        
+        if(flag==1){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+        
+        }
+        
+    }
+
+    return 0;
+    
+}
