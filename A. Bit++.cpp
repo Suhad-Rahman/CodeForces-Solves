f85e32cde461e08e8b4fdb95b19b656d4a7ee536
Suhad Rahman <suhadturan03@gmail.com>
2025-10-21 03:44:13 +0600

    A. Bit++
    https://codeforces.com/problemset/problem/282/A

---
+++ A. Bit++.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    
+    int x {0};
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        if (s[1] == '+') {
+            x++;
+        }
+        else {
+            x--;
+        }
+
+    }
+    
+    cout<<x<<endl;
+
+    return 0;
+}
