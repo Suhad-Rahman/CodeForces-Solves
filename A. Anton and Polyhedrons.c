18bb049e7cd24e27e2272ea75a907e3204ff069a
Suhad Rahman <suhadturan03@gmail.com>
2025-11-01 15:28:31 +0600

    A. Anton and Polyhedrons [C]
    Problem Link: https://codeforces.com/contest/785/problem/A

---
+++ A. Anton and Polyhedrons.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+    
+    char str[13];
+    int sum=0;
+    
+    for(int i=0; i<n; i++) {
+        scanf("%s", str);
+    
+        if(str[0]=='T'){
+            sum+=4;
+        }
+        
+        else if(str[0]=='C') {
+            sum+=6;
+        }
+        
+        else if(str[0]=='O'){
+            sum +=8;
+        }
+        
+        else if(str[0]=='D') {
+            sum +=12;
+        }
+        
+        else if(str[0] == 'I') {
+            sum +=20;
+        }
+        
+    }
+    
+    printf("%d", sum);
+    
+    
+    
+    return 0;
+}
