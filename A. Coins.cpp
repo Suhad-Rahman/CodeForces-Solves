8f3e723957621627dcb4224785a0e30d7166349a
Suhad Rahman <suhadturan03@gmail.com>
2025-12-09 00:32:30 +0600

    A. Coins [C++]
    Problem Link: https://codeforces.com/contest/1814/problem/A

---
+++ A. Coins.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+void solve() {
+    
+    long long n {}, k {};
+    cin>>n>>k;
+    
+    if(((n-k) % 2 == 0) || (n%2==0)){
+        cout<<"YES\n";
+    }
+    
+    else{
+        cout<<"NO\n";
+    }
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t {};
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
