b9974236b57134c65373b1662e139b4244d0e50b
Suhad Rahman <suhadturan03@gmail.com>
2026-08-08 16:50:23 +0600

    1371A - Magical Sticks
    Problem Link: https://codeforces.com/problemset/problem/1371/A

---
+++ A. Magical Sticks.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+void solve() {
+    
+    int n {};
+    cin>>n;
+    
+    cout<<((n & 1) ? n/2+1 : n/2)<<"\n";
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t {};
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
